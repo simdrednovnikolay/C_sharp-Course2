29e558c20da2b90b360c9871d2380fc4f9ccec91
Nikolay Rednov <red.niko@yandex.ru>
2020-11-04 10:50:48 +0400

    chek2
    сделал класс Rock т к класс BasrObj асбтрактный и реализация проиходит только в наследнике. Добавил корабль Ship , реализовал удаление объекта после попадания пули при помощи коллекции.

--- C_sharp_course2_lessons/Asteroid/Object/BaseObject.cs
+++ C_sharp_course2_lessons/Asteroid/Object/BaseObject.cs
@@ -4,16 +4,22 @@ using System.Windows.Forms;
 
 namespace Asteroid.Object
 {
-    class BaseObject
+
+    interface ICollision
+    {
+        Rectangle Rect { get; }
+        bool Collision(ICollision obj);
+    }
+     abstract class BaseObject: ICollision
     {
         protected Point pos, dir;
         protected Size size;
 
-        Image image = Image.FromFile("Picture\\rock.png");
+        
         protected Point Pos
         {
             get => pos;
-            set => pos=value;
+            set => pos = value;
         }
         protected Point Dir
         {
@@ -25,6 +31,9 @@ namespace Asteroid.Object
             get => size;
             set => size = value;
         }
+
+        public Rectangle Rect => new Rectangle(Pos, Size);
+
         public BaseObject(Point pos, Point dir, Size size)
         {
             this.pos = pos;
@@ -34,32 +43,141 @@ namespace Asteroid.Object
 
         public virtual void Draw()
         {
+            
+        }
+
+        public virtual void Update()
+        {
+            
+
+        }
+
+        public bool Collision(ICollision obj)
+        {
+            return obj.Rect.IntersectsWith(this.Rect);
+        }
+    }
+
+    class Rock : BaseObject
+    {
+        Image image = Image.FromFile("Picture\\unitaz.png");
+
+        public override void Draw()
+        {
             Game.Buffer.Graphics.DrawImage(image, new Point(Pos.X, Pos.Y));
         }
 
-        public void Update()
+        public override void Update()
         {
             pos.X = pos.X + dir.X;
             pos.Y = pos.Y + dir.Y;
-            if (pos.X < 0 | pos.X > Game.Width) dir.X = -dir.X;
-            if (pos.Y < 0 | pos.Y > Game.Height) dir.Y = -dir.Y;
+            if (pos.X < 0) dir.X = -dir.X;
+            if (pos.X > Game.Width) dir.X = -dir.X;
+            if (pos.Y < 0) dir.Y = -dir.Y;
+            if (pos.Y > Game.Height) dir.Y = -dir.Y;
+
+        }
+        public Rock(Point pos, Point dir, Size size): base(pos, dir, size)
+        {
 
         }
     }
 
-    class Star:BaseObject
+    class Star : BaseObject
     {
-        
-        Image image = Image.FromFile("Picture\\star.png");
 
-        public Star(Point pos, Point dir, Size size):base(pos,dir,size)
+        Image image = Image.FromFile("Picture\\banana.png");
+
+        public Star(Point pos, Point dir, Size size) : base(pos, dir, size)
         {
-            
+
         }
 
         public override void Draw()
         {
             Game.Buffer.Graphics.DrawImage(image, new Point(Pos.X, Pos.Y));
         }
+
+        public override void Update()
+        {
+            pos.X += dir.X;
+            if (pos.X < -image.Width)
+            {
+                pos.X = Game.Width + Game.rnd.Next(10, 100);
+                pos.Y = Game.rnd.Next(0, Game.Height);
+            }
+        }
+    }
+
+    
+    class Bullet: BaseObject
+    {
+        Point start;
+
+        Image image = Image.FromFile("Picture\\bumaga.png");
+        public Bullet(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+            start = pos;
+        }
+
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawImage(image, new Point(Pos.X, Pos.Y));
+        }
+
+        public override void Update()
+        {
+            Pos = new Point(Pos.X + Dir.X, Pos.Y);
+        }
+
+        public void Reset()
+        {
+            Pos = start;
+        }
+
+    }
+
+    class Ship:BaseObject
+    {
+        Point start;
+        Image image = Image.FromFile("Picture\\ship.png");
+
+        public int x => Pos.X;
+        public int y => Pos.Y;
+
+        public void Up()
+        {
+            Pos = new Point(Pos.X, Pos.Y-Dir.X);
+        }
+
+        public void Down()
+        {
+            Pos = new Point(Pos.X, Pos.Y + Dir.X);
+        }
+
+        public void Right()
+        {
+            Pos = new Point(Pos.X+Dir.X, Pos.Y );
+        }
+
+        public void Left()
+        {
+            Pos = new Point(Pos.X-Dir.X, Pos.Y);
+        }
+        public Ship(Point pos, Point dir, Size size): base(pos, dir, size)
+        {
+            start = pos;
+        }
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawImage(image, new Point(Pos.X, Pos.Y));
+        }
+
+        public override void Update()
+        {
+            Pos = new Point(Pos.X + Dir.X, Pos.Y);
+        }
+
+
     }
 }
